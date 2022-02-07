d4db4f50d63c1193f3cf0970cef886e6696c3306
chandresh <er.chandreshbhai@gmail.com>
2022-02-07 22:24:24 +0530

    seperating config file

--- express/db.js
+++ express/db.js
@@ -5,10 +5,12 @@ var mykey = crypto.createDecipher('aes-128-cbc', 'myHexKey');
 var pass = mykey.update('00999fc70e918d77fea4fd9634ce9945', 'hex', 'utf8')
 pass += mykey.final('utf8');
 
+var config = require('./config');
+
 var dbConn = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'qwerty@123',
+    password: config.dbPass,
     //password: pass,
     database: 'Apuesta'    
   });
--- express/mail.js
+++ express/mail.js
@@ -5,14 +5,18 @@ var mykey = crypto.createDecipher('aes-128-cbc', 'myHexKey');
 var pass = mykey.update('16f6ee333087cc134afc201236048cd5', 'hex', 'utf8')
 pass += mykey.final('utf8');
 
+var config = require('./config');
+
 var mail = nodemailer.createTransport({
     service: 'gmail',
     // host:'smtp.gmail.com',
     // port:465,
     // secure:true,
     auth: {
-        user: 'server.trademe@gmail.com',
-        pass: pass
+        // user: 'server.trademe@gmail.com',
+        user: config.emailId,
+        // pass: pass
+        pass: config.emailPass
     }
 });
 
