abd96e1aa4474dac7f7c89a98f3e7b6c16e9c64b
chandresh <er.chandreshbhai@gmail.com>
2022-02-08 22:33:57 +0530

    config file adding

---
+++ express/_config.js
@@ -0,0 +1,12 @@
+// remove _ from file name and edit credentials
+
+var config = {
+    dbPass: "",
+    emailId: "",
+    emailPass: "",
+    joloSoftBaseURL: "",
+    joloSoftAPIKey: "",
+    joloSoftUserId: ""
+};
+
+module.exports = config;
